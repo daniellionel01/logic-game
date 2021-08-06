8608b87d9e5ec9351ce03b04455d6619baca85be
soykai <danielkurz03@protonmail.com>
2021-08-06 21:31:06 +0200

    add border of empty cells around grid

--- src/components/Grid.vue
+++ src/components/Grid.vue
@@ -2,11 +2,10 @@
   <div id="grid">
     <div id="grid-container" :style="containerStyle">
       <template v-for="row in rows" :key="row">
-        <template v-for="col in rows" :key="col">
+        <template v-for="col in cols" :key="col">
           <Cell :row="row" :col="col" class="cell" />
         </template>
       </template>
-        <!--<Cell v-for="(cell, index) in cells" :key="index" :data="cell" :index="index" class="cell" />-->
     </div>
   </div>
 </template>
@@ -24,12 +23,16 @@ export default defineComponent({
   setup: () => {
     const store = useStore()
 
-    const rows = computed(() => Array.from(Array(store.getters.height).keys()).map(row => row + 1))
-    const cols = computed(() => Array.from(Array(store.getters.width).keys()).map(col => col + 1))
+    // we add +2 to width and height to have a border of empty cells around the grid
+    const width = computed(() => store.getters.width + 2)
+    const height = computed(() => store.getters.width + 2)
+
+    const rows = computed(() => Array.from(Array(height.value).keys()).map((_row, index) => index))
+    const cols = computed(() => Array.from(Array(width.value).keys()).map((_col, index) => index))
 
     const containerStyle = computed(() => ({
       'display': 'grid',
-      'gridTemplateColumns': Array.from(Array(store.getters.width).keys()).map(_ => "1fr").join(" "),
+      'gridTemplateColumns': Array.from(Array(width.value).keys()).map(_ => "1fr").join(" "),
       'gap': '5px'
     }))
 
