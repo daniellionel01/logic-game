dd793a0ed432934dddd1676226249ff9839fcb35
Daniel Kurz <danielkurz03@protonmail.com>
2022-03-04 21:16:22 +0100

    permutations vector

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,43 @@
-use std::thread;
-use std::time::Duration;
+//use std::thread;
+//use std::time::Duration;
 extern crate num_cpus;
 
+#[derive(Debug, Clone)]
 enum Direction {
     Up, Down, Left, Right
 }
+#[derive(Debug, Clone)]
 enum Color {
     Red, Green, Blue, None
 }
+#[derive(Debug, Clone)]
 enum InstructionType {
     Pass, Forward, RotateLeft, RotateRight,
     CallFunction, Paint
 }
 
+#[derive(Debug, Clone)]
 struct Instruction {
     instruction_type: InstructionType,
     color: Color,
     payload: u32,
 }
 
+#[derive(Debug, Clone)]
 struct Ship {
     x: i64,
     y: i64,
     dir: Direction
 }
 
+#[derive(Debug, Clone)]
 struct Cell {
     x: i64,
     y: i64,
     color: Color,
 }
 
+#[derive(Debug, Clone)]
 struct Star {
     x: i64,
     y: i64,
@@ -45,7 +52,6 @@ fn main() {
     let colors: Vec<Color> = vec![
         Color::None,
         Color::Red,
-        Color::Green,
         Color::Blue
     ];
     let instructions: Vec<InstructionType> = vec![
@@ -56,38 +62,44 @@ fn main() {
         InstructionType::CallFunction,
         InstructionType::Paint
     ];
-    let permutations: Vec<Instruction> = vec![];
-    for c in colors {
-        for i in instructions {
+    let mut permutations: Vec<Instruction> = vec![];
+    for c in &colors {
+        for i in &instructions {
             let instruction: Instruction = Instruction {
-                instruction_type: i,
-                color: c,
+                instruction_type: i.clone(),
+                color: c.clone(),
                 payload: 0
             };
-            permutations.push(instruction);
+            permutations.push(instruction.clone());
 
-            if i == InstructionType::CallFunction {
-                let inst: Instruction = instruction.copy
-                permutations.push(instruction);
+            let is_call = match i {
+                InstructionType::CallFunction => true,
+                _ => false
+            };
+            if is_call {
+                permutations.push(Instruction {
+                    payload: 1,
+                    ..instruction
+                });
             }
         }
     }
 
-    let mut total: i32 = 24;
-    total = total.pow(10);
+    println!("{}", permutations.len());
 
-    // permutations array
-    // total permutations
+    let mut total: usize = permutations.len();
+    total = total.pow(10);
+    println!("{}", total);
+
+    for perm in &permutations {
+        println!("{:?} {:?} {:?}", perm.instruction_type, perm.color, perm.payload);
+    }
+
+    let cores = num_cpus::get();
+    println!("cpu cores: {}", cores);
 
     // get count of cpu cores
     // spawn as many threads
     // each for one segment of total permutations
     // thread::spawn(|| { });
-
-    let cpus = num_cpus::get();
-    for i in 0..cpus {
-        println!("{}", i);
-    }
-
-    println!("{}", total)
 }
