8f1117be6de70df04c1e050145def06d8c7f5952
Daniel Kurz <danielkurz03@protonmail.com>
2022-03-04 09:28:49 +0100

    first lines

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,45 @@
+use std::thread;
+use std::time::Duration;
+extern crate num_cpus;
+
+enum Direction {
+    UP, DOWN, LEFT, RIGHT
+}
+enum Color {
+    RED, GREEN, BLUE, NONE
+}
+
+struct Ship {
+    x: i64,
+    y: i64,
+    dir: Direction
+}
+
+struct Cell {
+    x: i64,
+    y: i64,
+    color: Color,
+}
+
+struct Star {
+    x: i64,
+    y: i64,
+    collected: bool
+}
+
+fn game() {
+    let step: u32 = 0;
+}
+
+// permutations array
+// total permutations
+
 fn main() {
-    println!("Hello, world!");
+    // get count of cpu cores
+    // spawn as many threads
+    // each for one segment of total permutations
+    // thread::spawn(|| { });
+
+    let num = num_cpus::get();
+    println!("{}", num);
 }
