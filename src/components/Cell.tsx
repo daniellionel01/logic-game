bd65dd9282d452fe9a14e13dc27cad4d9deebe53
Daniel Kurz <danielkurz03@protonmail.com>
2022-11-28 11:56:38 +0100

    add and rotate ship

--- src/components/Cell.tsx
+++ src/components/Cell.tsx
@@ -31,14 +31,38 @@ const Cell: Component<CellProps> = (props: CellProps) => {
     return !!level().stars.find(s => s.row === row && s.col === col)
   })
 
+  const isShip = createMemo<boolean>(() => {
+    const s = level().ship
+    return s.col === col && s.row === row
+  })
+
+  const rotate = createMemo<string>(() => {
+    if (!isShip()) return ""
+
+    const s = level().ship
+    if (s.dir === "TOP") {
+      return "-rotate-90"
+    } else if (s.dir === "LEFT") {
+      return "rotate-180"
+    } else if (s.dir === "RIGHT") {
+      return "rotate-0"
+    } else if (s.dir === "BOTTOM") {
+      return "rotate-90"
+    }
+    throw new Error("Missing rotation value")
+  })
+
   return (
     <div
-      class={`w-14 border-2 rounded-2xl aspect-square ${bg()}`}
-      classList={{ "flex justify-center items-center text-white": isStar()}}
+      class={`w-14 border-2 rounded-2xl aspect-square ${bg()} ${rotate()}`}
+      classList={{ "flex justify-center items-center text-white": isStar() || isShip() }}
       style="box-shadow: 0 0 8px 0 rgba(0, 0, 0, 0.08), 0 0 20px 0 rgba(0, 0, 0, 0.05);"
     >
       <Show when={isStar()}>
         <i class="fas fa-star"></i>
+      </Show>
+      <Show when={isShip()}>
+        <i class="fas fa-space-shuttle"></i>
       </Show>
     </div>
   )
